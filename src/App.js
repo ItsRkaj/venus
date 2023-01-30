cb829db456c51d2560851bab8685547706e3df6d
Rikard Johansson <rikard01j@gmail.com>
2023-01-30 15:07:26 +0100

    Inplemented basic wireframe via Material UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import "./App.css";
-import React, { useState } from "react";
-import Day from "./components/Day";
-import Week from "./components/Week";
-import Month from "./components/Month";
+import React from "react";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
-  const [showComponent, setShowComponent] = useState("Day");
-
   return (
     <div className="App">
-      {showComponent === "Day" ? <Day /> : null}
-      {showComponent === "Week" ? <Week /> : null}
-      {showComponent === "Month" ? <Month /> : null}
-      <button onClick={() => setShowComponent("Day")}>Day</button>
-      <button onClick={() => setShowComponent("Week")}>Week</button>
-      <button onClick={() => setShowComponent("Month")}>Month</button>
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/dashboard/Deposits.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import Title from "./Title";
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+export default function Deposits() {
+  return (
+    <React.Fragment>
+      <Title>Current consumption</Title>
+      <Typography component="p" variant="h4">
+        124 kWh
+      </Typography>
+      <Typography color="text.secondary" sx={{ flex: 1 }}>
+        {new Date().getHours()}:{new Date().getMinutes()},{" "}
+        {new Date().getDate()}/{new Date().getDay()}-{new Date().getFullYear()}
+      </Typography>
+      <div>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          View more
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
