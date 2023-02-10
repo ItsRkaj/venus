7728fdc5652c7eb4558360d8b20de7a8add2c1fb
Rikard Johansson <rikard01j@gmail.com>
2023-02-10 13:56:53 +0100

    Introducing React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import React from "react";
-import Dashboard from "./components/dashboard/Dashboard";
+import Navbar from "./components/Navbar";
+import Day from "./components/Day";
+import Week from "./components/Week";
+import Month from "./components/Month";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Day />}></Route>
+            <Route path="/" element={<Week />}></Route>
+            <Route path="/" element={<Month />}></Route>
+            <Route path="*" element={<div>404 Page Not Found</div>} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
